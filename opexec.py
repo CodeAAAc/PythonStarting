025b4970ccbe7462fde4dfe8042b1f9f4291a4cc
Andrés Alberca <andres.al314@hotmail.com>
2024-05-22 10:34:41 -0500

    first script with operators

---
+++ opexec.py
@@ -0,0 +1,9 @@
+print ("Programa que recibe la hora actual, la duración de evento, y devuelve la hora de finalización del evento")
+
+hour = int(input("Hora de inicio (horas): "))
+mins = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+
+hours = int((mins + dura) / 60) + hour
+mins2 = (mins + dura) % 60
+print (hours, mins2, sep=":")
