72d5c11995ebfecc735ef801081dea02f7f42507
Andrés Alberca <andres.al314@hotmail.com>
2024-05-27 19:58:15 -0500

    Script using if,elif and else

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+planta = input("Introduce el nombre de la planta: \n")
+
+if planta == "Espatifilo":
+    print("Si, ¡El Espatifilo es la mejor planta de todos los tiempos!")
+elif planta == "espatifilo":
+    print("No, ¡quiero un gran Espatifilo!")
+else:
+    print("¡Espatifilo!, ¡No " + planta + "!")
