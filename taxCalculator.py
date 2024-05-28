2a6ea12679962e1093ad00598f47bd3db4965d23
Andrés Alberca <andres.al314@hotmail.com>
2024-05-27 20:16:47 -0500

    script to calculate the taxes

---
+++ taxCalculator.py
@@ -0,0 +1,11 @@
+ingreso = float(input("Introduce tu ingreso anual: "))
+
+if ingreso < 85528:
+	impuesto = ingreso* 0.18 - 556.02
+else:
+    impuesto = 14839.02 + (ingreso - 85528) * 0.32 
+
+if impuesto < 0.0:
+    impuesto = 0.0
+
+print("El impuesto es:", round(impuesto, 0), "pesos")
