52c3cf72383eadc0814a8c99be85e7cab94e9978
Andrés Alberca <andres.al314@hotmail.com>
2024-05-27 22:12:19 -0500

    Create a script for finishing a bucle with break

---
+++ interesant_bucle.py
@@ -0,0 +1,9 @@
+palabra = input("Ingrese la palabra secreta: ")
+
+while True:
+    palabra = input("Ingrese la palabra secreta: ")
+    if palabra == "chupacabra":
+        break
+
+print("Has dejado el bucle con éxito.")
+    
