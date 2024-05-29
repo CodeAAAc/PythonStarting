db50c978120363936c3c1f2f1b8823bee8e73948
Andrés Alberca <andres.al314@hotmail.com>
2024-05-29 11:20:42 -0500

    Create a script for show the names of the beatles

---
+++ beatles_list.py
@@ -0,0 +1,24 @@
+beatles = [];
+print(beatles);
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+print(beatles);
+
+for i in range(2):
+    nombre = input ("Ingrese el nombre del nuevo beatle: ");
+    beatles.append(nombre);
+print(beatles)
+
+del beatles[3]
+del beatles[3]
+
+print(beatles)
+
+beatles.insert(0,"Ringo Starr")
+print(beatles)
+
+
+print("Longitud de la list", len(beatles))
