7bce21e481036d76331fcc2be13feede53d954bd
Andrés Alberca <andres.al314@hotmail.com>
2024-05-30 13:13:09 -0500

    Change the limit for j index in second for

--- list_rep.py
+++ list_rep.py
@@ -1,12 +1,19 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 
-my_list2 = my_list[:]
-my_list3 =[]
+my_list2 = []
+long_list = len(my_list);
 
-for i in range(len(my_list2)):
-    for j in range(len(my_list2[i+1:])):
-        if my_list2[i] == my_list2[i+j+1]:
-            my_list2[j+1] = 0
+sw = True
+
+for i in range(long_list):
+    for j in range(i):
+        if my_list[i] == my_list[j]:
+            sw = False
+            break
+        else:
+            sw = True
+    if sw:
+        my_list2.append(my_list[i])
         
 print("La lista con elementos únicos:")
 print(my_list2)
