84993c5e948dcc31755425ff0f1caa963aeae6b4
Andrés Alberca <andres.al314@hotmail.com>
2024-05-31 10:21:00 -0500

    add test values to fuel script

--- fuel_mes.py
+++ fuel_mes.py
@@ -11,3 +11,10 @@ def miles_gallon_to_liters_100km(miles):
     
     return liters/km
 
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
+
