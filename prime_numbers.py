9534a0c5e550ef624c026d18f3d775acc284e4bd
Andrés Alberca <andres.al314@hotmail.com>
2024-05-31 10:06:01 -0500

    Create a script to check prime number

---
+++ prime_numbers.py
@@ -0,0 +1,9 @@
+def is_prime(num):
+    for div in range(2,num):
+        if num % div == 0:
+            return False
+    return True        
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
